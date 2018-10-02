6a939a9555ff45c88cc5e58f7ea3c20b2e7935a1
Otavio Parraga <otavio.parraga@acad.pucrs.br>
2018-10-02 15:52:56 -0300

    ihc questions implemented

--- php/accessScript.php
+++ php/accessScript.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$archive = fopen("../data/test.txt", "a+");
+$archive = fopen("../data/accessData", "w");
 fwrite($archive, "Nome: " . $_SESSION["name"] . "\n");
 fwrite($archive, "Matricula: " . $_SESSION["userId"] . "\n");
 fwrite($archive, "Desempenho: " . $_POST['trials'] . "\n");
